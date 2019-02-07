287478c2adb1cefd73660f5bd310f5aed1c9ab8f
Tibi Craciun <tibi@happysoft.ro>
2019-02-08 00:02:35 +0200

    downgrade backup scripts to 0.16.3 to 0.16.3

--- scripts/restore_backup.sh
+++ scripts/restore_backup.sh
@@ -5,13 +5,15 @@ BACKUP_FILE=$1
 
 RESTORE_DATE=`date +%Y_%m_%d_%H_%M`
 
-cp  /root/.bitcoin/wallet.dat /root/.bitcoin/wallet.dat-backup-${RESTORE_DATE}
+# cp  /root/.bitcoin/wallet.dat /root/.bitcoin/wallet.dat-backup-${RESTORE_DATE}
+cp  /root/.bitcoin/wallets/wallet.dat /root/.bitcoin/wallets/wallet.dat-backup-${RESTORE_DATE}
 
 rm -rf ${BACKUP_FOLDER}
 mkdir -p ${BACKUP_FOLDER}
 
 tar -xzf ${BACKUP_FILE} -C ${BACKUP_FOLDER}/
-cp ${BACKUP_FOLDER}/wallet.dat /root/.bitcoin/wallet.dat
+# cp ${BACKUP_FOLDER}/wallet.dat /root/.bitcoin/wallet.dat
+cp ${BACKUP_FOLDER}/wallet.dat /root/.bitcoin/wallets/wallet.dat
 
 
 rm -rf ${BACKUP_FOLDER}
